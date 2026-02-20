44f8cda7cda75d64efe52841d108cdd4f37e5d39
CyaNabe <abe.mitsunobu@mail.kyutech.jp>
2026-02-21 06:01:11 +0900

    named /odom odom topic name

--- ros_ws/src/bringup/launch/main.launch.py
+++ ros_ws/src/bringup/launch/main.launch.py
@@ -56,7 +56,7 @@ def generate_launch_description():
     odom_to_base = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'odom_rf2o', 'base_link'],
+        arguments=['0', '0', '0', '0', '0', '0', 'odom', 'base_link'],
         #parameters=[{'use_sim_time': True}]
     )
  
